21a33119d9650b2515c1c22d9ed382e3f1bef500
irastrazdina <irastrazdina@gmail.com>
2019-12-26 22:47:11 +0300

    Lesson_03 tasks added

---
+++ Lesson_03/L3T2.py
@@ -0,0 +1,2 @@
+a = ['Email:', 'SSN:','Address:','Home Phone:','Mobile Phone: ','DOB:','Date of Surgery:','Date of Service:','Facility of Service:','Clinic Number:','Employer:','Work Phone: ','Fax: ','Type:','IPA:','Health Plan:','ID #:','Claims Address:','Group #:','Claim # / PO #:','Phone:','Fax:','Contact','Adjuster Email','Util Review Phone','Util Review Fax','Doctor:','NPI #: ','Date of Injury: ','Body Parts:','Body Part Side:','Gender:','Diagnosis:','Diagnosis 2:','Procedure:']
+print(a[-2])
---
+++ Lesson_03/L3T3.py
@@ -0,0 +1,3 @@
+a = input("Enter a : ")
+print(int(a), int(a * 2), int(a * 3), int(a * 4))
+print(int(a) + int(a * 2) - int(a * 3) * int(a * 4))
